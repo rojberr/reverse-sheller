7751ffeb23d6e1d8741a3f99836669b498ebae90
rojberr <rojberr@outlook.com>
2022-10-26 19:56:44 +0200

    feat(server): Listen on hardcoded 4444 port

--- src/server/src/dev/drzymala/server/ServerSheller.java
+++ src/server/src/dev/drzymala/server/ServerSheller.java
@@ -1,8 +1,15 @@
 package dev.drzymala.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerSheller {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println();
         System.out.print(" _                                    \n");
@@ -13,5 +20,24 @@ public class ServerSheller {
         System.out.print("|_| |_|\\__,_/_/\\_\\___/\\___|_|    \\_/  \n");
         System.out.print("                                      \n");
         System.out.println();
+
+        listen();
+    }
+
+    private static void listen() throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(4444);
+        Socket clientSocket = serverSocket.accept();
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            if (".".equals(inputLine)) {
+                out.println("good bye");
+                break;
+            }
+            out.println(inputLine);
+        }
     }
 }
