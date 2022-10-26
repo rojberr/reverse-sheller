739f769bab7af6f0512ca88adb9856e8c4d05024
rojberr <rojberr@outlook.com>
2022-10-26 19:39:31 +0200

    feat(server): Print hax0rserv when server .jar executed

--- src/server/src/dev/drzymala/server/ServerSheller.java
+++ src/server/src/dev/drzymala/server/ServerSheller.java
@@ -3,6 +3,15 @@ package dev.drzymala.server;
 public class ServerSheller {
 
     public static void main(String[] args) {
-        System.out.println("Hello world! ");
+
+        System.out.println();
+        System.out.print(" _                                    \n");
+        System.out.print("| |                                   \n");
+        System.out.print("| |__   __ ___  _____  ___ _ ____   __\n");
+        System.out.print("| '_ \\ / _` \\ \\/ / __|/ _ \\ '__\\ \\ / /\n");
+        System.out.print("| | | | (_| |>  <\\__ \\  __/ |   \\ V / \n");
+        System.out.print("|_| |_|\\__,_/_/\\_\\___/\\___|_|    \\_/  \n");
+        System.out.print("                                      \n");
+        System.out.println();
     }
 }
