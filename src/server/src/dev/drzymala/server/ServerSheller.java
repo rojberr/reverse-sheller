7cd009f9cc39cbdc271d377a11b799a6b7f57294
Rojberr <rojberr@outlook.com>
2022-10-29 09:09:25 +0200

    feat(server): Redirect stdout and stdin, allow server->client cmd execution
    Relates: #NO-TICKET

--- src/server/src/dev/drzymala/server/ServerSheller.java
+++ src/server/src/dev/drzymala/server/ServerSheller.java
@@ -1,15 +1,21 @@
 package dev.drzymala.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerSheller {
 
-    public static void main(String[] args) throws IOException {
+    int port;
+
+    public ServerSheller(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
 
         System.out.println();
         System.out.print(" _                                    \n");
@@ -21,23 +27,31 @@ public class ServerSheller {
         System.out.print("                                      \n");
         System.out.println();
 
-        listen();
+        do {
+            listen(4444);
+        } while (true);
     }
 
-    private static void listen() throws IOException {
+    private static void listen(int port) throws Exception {
+        System.err.println("Listening at port " + port);
+        ServerSocket serverSocket = new ServerSocket(port);
+        Socket socket = serverSocket.accept();
+        System.err.println("Accepted");
+        transferStreams(socket);
+    }
 
-        ServerSocket serverSocket = new ServerSocket(4444);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            if (".".equals(inputLine)) {
-                out.println("good bye");
-                break;
-            }
-            out.println(inputLine);
-        }
+    private static void transferStreams(Socket socket) throws IOException,
+            InterruptedException {
+        InputStream systemInput = System.in;
+        OutputStream socketOutput = socket.getOutputStream();
+        InputStream socketInput = socket.getInputStream();
+        PrintStream output2 = System.out;
+        Thread thread1 = new Thread(new StreamTransferer(systemInput, socketOutput));
+        Thread thread2 = new Thread(new StreamTransferer(socketInput, output2));
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        socket.shutdownOutput();
+        thread2.join();
     }
 }
